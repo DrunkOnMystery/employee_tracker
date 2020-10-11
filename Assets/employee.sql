44a089d6723cfdb0eb6727e43acef61e55149a74
Ben Brumby <bbrumby00@gmail.com>
2020-10-11 15:33:26 -0400

    sql?

--- Assets/employee.sql
+++ Assets/employee.sql
@@ -5,18 +5,18 @@ CREATE DATABASE employees_db
 USE employees_db
 
 CREATE TABLE employee (
-firstname VARCHAR(30) NOT NULL
-lastname VARCHAR(30) NOT NULL
-role_id INTEGER(10) 
+firstname VARCHAR(30) NOT NULL,
+lastname VARCHAR(30) NOT NULL,
+role_id INTEGER(10),
 manager_id INTEGER(10)
 )
 
 CREATE TABLE role (
-    title VARCHAR(30) NOT NULL
-    salary DECIMAL(10)
+    title VARCHAR(30) NOT NULL,
+    salary DECIMAL(10),
     department_id INTEGER(10)
 )
 
 CREATE TABLE department (
-
+    department_name VARCHAR(30)
 )
