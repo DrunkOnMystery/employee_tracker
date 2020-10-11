80e8e2ac7bb338ef63be901bc72d8d6a13df175b
Ben Brumby <bbrumby00@gmail.com>
2020-10-10 21:19:35 -0400

    Wrote out prompts. Going to separate into multiple files

--- Assets/Startup/index.js
+++ Assets/Startup/index.js
@@ -2,7 +2,7 @@
 const inquirer = require("inquirer");
 const mysql = require("mysql");
 const logo = require("logo");
-const console.table = require("console.table")
+const console = require("console.table")
 
 
 //function init()
@@ -25,37 +25,49 @@ function loadPrompts() {
             value: "VIEW_EMPLOYEES"
         },
         {
-            name: "View All Departments",
+            name: "View Departments",
             value: "VIEW_DEPARTMENTS"
         },
-        {
-            name: "View All Roles",
-            value: "VIEW_ROLES"
-        },
-        {
-            name: "Update Employee Roles",
-            value: "UPDATE_ROLES"
-        },
-        {
-            name: "Update Employe Managers",
-            value: "UPDATE_MANAGERS"
-        },
-        {
-            name: "List Employees By Manager",
-            value: "EMPLOYEES_MANAGER"
-        },
-        {
-            name: "Delete Department",
-            value: "DELETE_DEPARTMENT"
-        },
-        {
-            name: "Delete roles",
-            value: "DELETE_ROLES"
-        },
-        {
-            name: "Delete Employees",
-            value: "DELETE_EMPLOYEES"
-        },
+        // {
+        //     name: "View All Roles",
+        //     value: "VIEW_ROLES"
+        // },
+        // {
+        //     name: "Add department",
+        //     value: "ADD_DEPARTMENT"
+        // },
+        // {
+        //     name: "Add Role",
+        //     value: "ADD_ROLE"
+        // },
+        // {
+        //     name: "Add Employee",
+        //     value: "ADD_EMPLOYEE"
+        // },
+        // {
+        //     name: "Update Employee Roles",
+        //     value: "UPDATE_ROLES"
+        // },
+        // {
+        //     name: "Update Employe Managers",
+        //     value: "UPDATE_MANAGERS"
+        // },
+        // {
+        //     name: "List Employees By Manager",
+        //     value: "EMPLOYEES_MANAGER"
+        // },
+        // {
+        //     name: "Delete Department",
+        //     value: "DELETE_DEPARTMENT"
+        // },
+        // {
+        //     name: "Delete roles",
+        //     value: "DELETE_ROLES"
+        // },
+        // {
+        //     name: "Delete Employees",
+        //     value: "DELETE_EMPLOYEES"
+        // },
         {
             name: "View Total Budget",
             value: "VIEW_BUDGET"
@@ -67,6 +79,12 @@ function loadPrompts() {
     switch (choice) {
         case "VIEW_EMPLOYEES":
             return viewEmployees();
+
+        case "VIEW_DEPARTMENTS":
+            return viewDepartments();
+        
+        case "VIEW_BUDGET":
+            return viewBudget();
     }
 
 
